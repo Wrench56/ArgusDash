7cf3176c5e5c757c04c2947f9a99dfc18a3a1361
Mark Devenyi <markdevenyidev@gmail.com>
2024-05-12 16:24:36 -0400

    Run cleanup function on main process termination

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,15 +1,37 @@
 import logging
 import logging.config
 
+import atexit
+import signal
+import sys
+import subprocess
+
 from server import start_server
 
 def main() -> None:
     init_logger()
-    start_server.init()
+    proc = start_server.init()
     
+    # Register cleanup function
+    atexit.register(lambda: cleanup(proc))
+    signal.signal(signal.SIGTERM, lambda: cleanup(proc))
+    signal.signal(signal.SIGINT, lambda: cleanup(proc))
+
+    # Block
+    proc.wait()
+
 def init_logger():
     logging.config.fileConfig('config/logger.config.ini')
     logging.info('Logging init done')
+
+def cleanup(process: subprocess.Popen) -> None:
+    logging.info('Cleanup function called')
+    process.terminate()
+    process.wait()
+    logging.info('Cleanup successful')
+    
+    # Raises SystemExit
+    sys.exit()
 
 if __name__ == '__main__':
     main()
