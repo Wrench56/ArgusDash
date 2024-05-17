3ac23b13049f4422dec6fd11b42d4b59dd075cdf
Mark Devenyi <markdevenyidev@gmail.com>
2024-05-17 19:34:14 -0400

    Add `favicon.png`

--- src/backend/server/main.py
+++ src/backend/server/main.py
@@ -8,3 +8,7 @@ app.mount('/assets', StaticFiles(directory='../public/assets'), name='static')
 @app.get('/', response_class=FileResponse)
 async def home():
     return '../public/login.html'
+
+@app.get('/favicon.png', include_in_schema=False)
+async def favicon():
+    return FileResponse('../public/favicon.png')
