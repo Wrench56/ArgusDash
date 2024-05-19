4dfa7e877585e2b4b4ddf4f2d92a73e004f3afd3
Wrench56 <dmarkreg@gmail.com>
2024-05-19 00:14:10 -0400

    Add `Version.svelte` login component

---
+++ src/frontend/src/lib/components/login/motd/Version.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import version from "$lib/version";
+    /* Update this */
+    const HEADER_LENGTH = 55;
+
+    const formatted_version = `${"".padStart(HEADER_LENGTH-version.length-1, " ")}v${version}`;
+</script>
+
+<pre>{formatted_version}</pre>
+
+<style>
+    pre {
+        margin: 0px 0px 16px 0px;
+        font-family: monaco, Consolas, "Lucida Console", monospace;
+        color: dimgray;
+    }
+</style>
