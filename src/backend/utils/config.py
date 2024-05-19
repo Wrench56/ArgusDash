2bd33d2ea5f4d526667ab02d5f18e87daea9a2d7
Mark Devenyi <markdevenyidev@gmail.com>
2024-05-19 02:55:13 -0400

    Optimize `config.py`

--- src/backend/utils/config.py
+++ src/backend/utils/config.py
@@ -2,14 +2,13 @@ from configparser import ConfigParser
 from typing import Any, Optional
 import logging
 
-_config: ConfigParser = None
+_config: ConfigParser = ConfigParser()
+
 
 def load() -> None:
-    global _config
-    
-    _config = ConfigParser()
     _config.read('config/config.ini')
     logging.info('Config loaded')
+
 
 def get(field: str) -> Optional[Any]:
     return _config[field]
