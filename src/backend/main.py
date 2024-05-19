29910ca4140ebf887020370afb3e1ab79752eb83
Wrench56 <dmarkreg@gmail.com>
2024-05-19 04:44:33 -0400

    Add `Status.svelte` login component

--- src/backend/main.py
+++ src/backend/main.py
@@ -5,16 +5,19 @@ import atexit
 import signal
 import sys
 
-from utils import processes, config
-
 from server import build, start
+from utils import config, processes, status
+
 
 def main() -> None:
     # Initialize logging
     init_logger()
-    
+
     # Load config.ini
     config.load()
+
+    # Cache status
+    status.update()
 
     # Register cleanup functions
     atexit.register(cleanup)
@@ -27,15 +30,18 @@ def main() -> None:
     # Start the ASGI uvicorn server
     start.run()
 
+
 def init_logger():
     logging.config.fileConfig('config/logger.config.ini')
     logging.info('Logging init done')
+
 
 def cleanup() -> None:
     processes.terminate_subprocesses()
 
     # Raises SystemExit
     sys.exit()
+
 
 if __name__ == '__main__':
     main()
--- src/backend/server/main.py
+++ src/backend/server/main.py
@@ -1,14 +1,23 @@
 from fastapi import FastAPI
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, ORJSONResponse
 from fastapi.staticfiles import StaticFiles
- 
+
+from utils import status
+
 app = FastAPI()
 app.mount('/assets', StaticFiles(directory='../public/assets'), name='static')
+
 
 @app.get('/', response_class=FileResponse)
 async def home():
     return '../public/login.html'
 
+
 @app.get('/favicon.png', include_in_schema=False)
-async def favicon():
+async def favicon() -> FileResponse:
     return FileResponse('../public/favicon.png')
+
+
+@app.get('/login/status', response_class=ORJSONResponse)
+async def login_info() -> ORJSONResponse:
+    return ORJSONResponse(status.get())
