43061465d5bbd273cabd938598d6f4a24c615527
Wrench56 <dmarkreg@gmail.com>
2024-05-19 02:24:57 -0400

    Fix `remove_subprocess` in `processes.py`

--- src/backend/utils/processes.py
+++ src/backend/utils/processes.py
@@ -1,24 +1,19 @@
 import logging
 import subprocess
 from collections import deque
-from typing import Deque, Tuple
+from typing import Deque
 
 _processes: Deque[subprocess.Popen] = deque()
 
 
-def add_subprocess(subproc: subprocess.Popen) -> Tuple[subprocess.Popen, int]:
+def add_subprocess(subproc: subprocess.Popen) -> subprocess.Popen:
     logging.info(f'Subprocess "{subproc}" created')
     _processes.append(subproc)
-    return (subproc, len(_processes) - 1)
+    return subproc
 
 
-def remove_subprocess(index: int) -> None:
-    logging.info(f"Removing subprocess")
-    if len(_processes) - 1 == index:
-        _processes.pop()
-        return
-
-    _processes.remove(_processes[index])
+def remove_subprocess(subproc: subprocess.Popen) -> None:
+    _processes.remove(subproc)
 
 
 def terminate_subprocesses() -> None:
