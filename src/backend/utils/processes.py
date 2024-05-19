09d87ac707ebc3ddd469758068c88dc0371d17bc
Mark Devenyi <markdevenyidev@gmail.com>
2024-05-19 01:01:05 -0400

    Fix subprocess handling & run `uvicorn` directly

--- src/backend/utils/processes.py
+++ src/backend/utils/processes.py
@@ -1,13 +1,18 @@
+import logging
+import subprocess
 from typing import List
 
-import subprocess
-
 _processes: List[subprocess.Popen] = []
+
+
 def add_subprocess(subproc: subprocess.Popen) -> subprocess.Popen:
+    logging.info(f'Subprocess "{subproc}" created')
     _processes.append(subproc)
     return subproc
 
+
 def terminate_subprocesses() -> None:
     for subproc in _processes:
+        logging.info(f'Terminating "{subproc}"')
         subproc.terminate()
         subproc.wait()
