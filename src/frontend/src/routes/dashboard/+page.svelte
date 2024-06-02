09742684a708103eb3e5db81a92a8f02c6cf5e7d
Mark Devenyi <markdevenyidev@gmail.com>
2024-06-02 07:04:31 -0400

    Add basic `dashboard.html` page

---
+++ src/frontend/src/routes/dashboard/+page.svelte
@@ -0,0 +1,18 @@
+<h1>Hello World!</h1>
+<p>This page is protected by server-side authentication.</p>
+
+<style>
+    h1 {
+        color: whitesmoke;
+        font-family: monaco, Consolas, "Lucida Console", monospace;
+    }
+
+    p {
+        color: whitesmoke;
+        font-family: monaco, Consolas, "Lucida Console", monospace;
+    }
+
+    :root {
+        background-color: black;
+    }
+</style>
