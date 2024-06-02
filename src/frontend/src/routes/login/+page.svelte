a55fde9fef5d4ccb3894dcf2d45696c6b6f2148b
Wrench56 <dmarkreg@gmail.com>
2024-06-02 07:02:17 -0400

    Add `login.html` page

--- src/frontend/src/routes/login/+page.svelte
+++ src/frontend/src/routes/login/+page.svelte
@@ -1,1 +1,20 @@
-<p>Hello World</p>
+<script>
+  import Input from "$lib/components/login/Input.svelte";
+import Header from "$lib/components/login/motd/Header.svelte";
+  import Motd from "$lib/components/login/motd/Motd.svelte";
+  import Status from "$lib/components/login/motd/Status.svelte";
+  import Version from "$lib/components/login/motd/Version.svelte";
+
+</script>
+
+<Header />
+<Version />
+<Motd />
+<Status />
+<Input />
+
+<style>
+    :root {
+        background-color: black;
+    }
+</style>
