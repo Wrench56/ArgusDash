96ff5fa997177e8c44523e82a8712c9efc4d9a69
Wrench56 <dmarkreg@gmail.com>
2024-06-02 07:04:46 -0400

    Add basic `blocked.html` page

---
+++ src/frontend/src/routes/blocked/+page.svelte
@@ -0,0 +1,18 @@
+<h1>Blocked</h1>
+<p>You are not logged in!</p>
+
+<style>
+    h1 {
+        color: whitesmoke;
+        font-family: monaco, Consolas, "Lucida Console", monospace;
+    }
+
+    p {
+        color: whitesmoke;
+        font-family: monaco, Consolas, "Lucida Console", monospace;
+    }
+
+    :root {
+        background-color: black;
+    }
+</style>
