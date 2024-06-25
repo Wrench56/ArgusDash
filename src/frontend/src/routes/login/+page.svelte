7dd2c8a579473309b7c29ed53b476e5611f0f0c2
Wrench56 <dmarkreg@gmail.com>
2024-06-25 02:27:10 -0400

    Fix formatting for `+page.svelte` of `login`

--- src/frontend/src/routes/login/+page.svelte
+++ src/frontend/src/routes/login/+page.svelte
@@ -1,6 +1,6 @@
-<script>
+<script lang="ts">
   import Input from "$lib/components/login/Input.svelte";
-import Header from "$lib/components/login/motd/Header.svelte";
+  import Header from "$lib/components/login/motd/Header.svelte";
   import Motd from "$lib/components/login/motd/Motd.svelte";
   import Status from "$lib/components/login/motd/Status.svelte";
   import Version from "$lib/components/login/motd/Version.svelte";
