1e6f0e1abdbd3010a3745fa502e4ecf4c9df729b
Wrench56 <dmarkreg@gmail.com>
2024-06-25 21:05:33 -0400

    Fix indentation for `login` components

--- src/frontend/src/lib/components/login/motd/Motd.svelte
+++ src/frontend/src/lib/components/login/motd/Motd.svelte
@@ -1,22 +1,22 @@
 <script lang="ts">
-    import { onMount } from "svelte";
-    
-    let motd = "Loading...";
+	import { onMount } from "svelte";
+	
+	let motd = "Loading...";
 
-    onMount(() => {
-      fetch(`${window.location.origin}/login/motd`)
-        .then(response => response.text())
-        .then((responseText) => motd = responseText);
-    });
+	onMount(() => {
+		fetch(`${window.location.origin}/login/motd`)
+			.then(response => response.text())
+			.then((responseText) => motd = responseText);
+	});
 </script>
 
 <div class="motd">{@html motd}</div>
 
 <style>
-    .motd {
-        margin-bottom: 20px;
-        font-family: monaco, Consolas, "Lucida Console", monospace;
-        color: whitesmoke;
-        white-space: pre;
-    }
+  .motd {
+    margin-bottom: 20px;
+    font-family: monaco, Consolas, "Lucida Console", monospace;
+    color: whitesmoke;
+    white-space: pre;
+  }
 </style>
--- src/frontend/src/lib/components/login/motd/Version.svelte
+++ src/frontend/src/lib/components/login/motd/Version.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
-    import version from "$lib/version";
-    /* Update this */
-    const HEADER_LENGTH = 55;
+  import version from "$lib/version";
+  /* Update this */
+  const HEADER_LENGTH = 55;
 
-    const formatted_version = `${"".padStart(HEADER_LENGTH-version.length-1, " ")}v${version}`;
+  const formatted_version = `${"".padStart(HEADER_LENGTH-version.length-1, " ")}v${version}`;
 </script>
 
 <pre>{formatted_version}</pre>
 
 <style>
     pre {
-        margin: 0px 0px 16px 0px;
-        font-family: monaco, Consolas, "Lucida Console", monospace;
-        color: dimgray;
+      margin: 0px 0px 16px 0px;
+      font-family: monaco, Consolas, "Lucida Console", monospace;
+      color: dimgray;
     }
 </style>
