1c5d98edebc481266ac8ee25755a42127da9d9e0
Mark Devenyi <markdevenyidev@gmail.com>
2024-06-25 21:05:33 -0400

    Fix indentation for `login` components

--- src/frontend/src/lib/components/login/motd/Header.svelte
+++ src/frontend/src/lib/components/login/motd/Header.svelte
@@ -9,9 +9,9 @@
 </pre>
 
 <style>
-    pre {
-        margin: 0px;
-        font-family: monaco, Consolas, "Lucida Console", monospace;
-        color: whitesmoke;
-    }
+	pre {
+		margin: 0px;
+		font-family: monaco, Consolas, "Lucida Console", monospace;
+		color: whitesmoke;
+	}
 </style>
