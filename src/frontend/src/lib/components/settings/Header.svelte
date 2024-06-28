f111c810b2ec69c12bb017de5ae2cc67936eb7b4
Wrench56 <dmarkreg@gmail.com>
2024-06-28 05:23:02 -0400

    Fix "Settings" header overflow

--- src/frontend/src/lib/components/settings/Header.svelte
+++ src/frontend/src/lib/components/settings/Header.svelte
@@ -1,16 +1,22 @@
-<pre>
+<div class="container">
+  <pre>
    _____      __  __  _                 
   / ___/___  / /_/ /_(_)___  ____ ______
   \__ \/ _ \/ __/ __/ / __ \/ __ `/ ___/
  ___/ /  __/ /_/ /_/ / / / / /_/ (__  ) 
 /____/\___/\__/\__/_/_/ /_/\__, /____/  
                           /____/        
-</pre>
+	</pre>
+</div>
 
 <style>
-	pre {
-		margin: -10px 0px 0px 0px;
-		font-family: monaco, Consolas, "Lucida Console", monospace;
-		color: whitesmoke;
-	}
+  pre {
+    margin: -10px 0px 0px 0px;
+    font-family: monaco, Consolas, "Lucida Console", monospace;
+    color: whitesmoke;
+  }
+
+  .container {
+    overflow: hidden;
+  }
 </style>
