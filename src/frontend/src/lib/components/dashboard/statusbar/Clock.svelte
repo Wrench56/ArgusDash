5a1bdde30ed9eac60cc05067146c0d1e7eb630f0
Mark Devenyi <markdevenyidev@gmail.com>
2024-06-28 05:39:48 -0400

    Add `Clock.svelte` widget to `Statusbar.svelte`

---
+++ src/frontend/src/lib/components/dashboard/statusbar/Clock.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  setInterval(showTime, 1000);
+
+  let shour = "00";
+  let smin = "00";
+  let ssec = "00";
+  let am_pm = "AM";
+
+  function showTime() {
+    let time = new Date();
+    let hour = time.getHours();
+    let min = time.getMinutes();
+    let sec = time.getSeconds();
+    am_pm = "AM";
+
+    if (hour >= 12) {
+      if (hour > 12) hour -= 12;
+      am_pm = "PM";
+    } else if (hour == 0) {
+      am_pm = "AM";
+    }
+
+    shour = hour < 10 ? "0" + hour : hour.toString();
+    smin = min < 10 ? "0" + min : min.toString();
+    ssec = sec < 10 ? "0" + sec : sec.toString();
+  }
+
+  showTime();
+</script>
+
+<pre>
+{`${shour}:${smin}:${ssec} ${am_pm}`}
+</pre>
+
+<style>
+  pre {
+    color: whitesmoke;
+    font-family: monaco, Consolas, "Lucida Console", monospace;
+    font-size: 12px;
+    margin: 0px;
+    display: grid;
+    grid-column: 2;
+  }
+</style>
---
+++ src/frontend/src/lib/components/dashboard/statusbar/Statusbar.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+  import version from "$lib/version";
+  import Clock from "./Clock.svelte";
+</script>
+
+<div class="container">
+  <a href="/settings">
+    <pre class="version"> ArgusDash - v{version}</pre>
+  </a>
+  <Clock></Clock>
+</div>
+
+<style>
+  .container {
+    position: fixed;
+    width: 100%;
+    height: 18px;
+    overflow: hidden;
+    bottom: 0px;
+    left: 0px;
+    right: 0px;
+    color: whitesmoke;
+    font-family: monaco, Consolas, "Lucida Console", monospace;
+    padding: 0px;
+    background-color: #070707;
+    align-items: center;
+    text-align: center;
+    display: inline-grid;
+    column-gap: 2px;
+    grid-template-columns: 180px auto;
+  }
+
+  pre {
+    color: whitesmoke;
+    font-family: monaco, Consolas, "Lucida Console", monospace;
+    font-size: 12px;
+    margin: 0px;
+    position: absolute;
+    top: 50%;
+    transform: translate(0%, -50%);
+  }
+
+  .version {
+    grid-column: 1;
+    padding: 4px;
+  }
+
+  .version:hover {
+    background-color: rgb(102, 153, 255);
+  }
+</style>
