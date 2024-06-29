188859ad0d818ba491870044c6d4bc0ac77b5e02
Wrench56 <dmarkreg@gmail.com>
2024-06-28 23:39:26 -0400

    Add `BuildVersion.svelte`

---
+++ src/frontend/src/lib/components/dashboard/statusbar/BuildVersion.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import version from "$lib/version";
+</script>
+
+<a href="/settings">
+  <pre class="version"> ArgusDash - v{version}</pre>
+</a>
+
+<style>
+  pre {
+    color: whitesmoke;
+    font-family: monaco, Consolas, "Lucida Console", monospace;
+    font-size: 12px;
+    margin: 0px;
+  }
+
+  .version {
+    grid-column: 1;
+    padding: 4px;
+  }
+
+  a {
+    flex-grow: 1;
+    text-decoration: none;
+  }
+
+  .version:hover {
+    background-color: rgb(102, 153, 255);
+  }
+</style>
