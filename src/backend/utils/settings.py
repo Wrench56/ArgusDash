9f1c86229770a0b81ff1144cf81fc5701129426d
Mark Devenyi <markdevenyidev@gmail.com>
2024-07-01 05:05:32 -0400

    Implement basic `feature_build` functionalities

--- src/backend/utils/settings.py
+++ src/backend/utils/settings.py
@@ -1,19 +1,29 @@
-from typing import List
+from typing import Dict, List, Optional
 
-_settings: List[str] = ['E', 'E']
+from utils import feature_build
+
+_settings: Dict[str, str] = {
+    'BUILD_MODE': 'E',
+    'STATUS_IN_SETTINGS': 'E'
+}
 
 
-def update_setting(id_: int, value: str) -> bool:
+def update_setting(id_: str, value: str) -> bool:
+    if id_ in feature_build.get_features():
+        if value == 'E':
+            feature_build.enable_feature(id_)
+        else:
+            feature_build.disable_feature(id_)
     _settings[id_] = value
     return True
 
 
 def get_all() -> List[str]:
-    return _settings.copy()
+    return list(_settings.values())
 
 
-def get_setting(id_: int) -> str:
-    return _settings[id_]
+def get_setting(id_: str) -> Optional[str]:
+    return _settings.get(id_)
 
 
 def get_default() -> List[str]:
