7ee7dd907746c9d5a388b4430dddb46c5afaa8fa
Wrench56 <dmarkreg@gmail.com>
2024-07-08 03:31:13 -0400

    Add `plugins` page

---
+++ src/frontend/src/lib/components/plugins/Header.svelte
@@ -0,0 +1,22 @@
+<div class="container">
+  <pre>
+    ____  __            _           
+   / __ \/ /_  ______ _(_)___  _____
+  / /_/ / / / / / __ `/ / __ \/ ___/
+ / ____/ / /_/ / /_/ / / / / (__  ) 
+/_/   /_/\__,_/\__, /_/_/ /_/____/  
+              /____/                
+      </pre>
+</div>
+
+<style>
+  pre {
+    margin: -10px 0px 0px 0px;
+    font-family: monaco, Consolas, "Lucida Console", monospace;
+    color: whitesmoke;
+  }
+
+  .container {
+    overflow: hidden;
+  }
+</style>
---
+++ src/frontend/src/routes/plugins/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import Box from "$lib/components/plugins/Box.svelte";
+  import Header from "$lib/components/plugins/Header.svelte";
+  import UrlField from "$lib/components/plugins/UrlField.svelte";
+  import Separator from "$lib/components/shared/Separator.svelte";
+  import Statusbar from "$lib/components/shared/statusbar/Statusbar.svelte";
+  import DNotificationContainer from "$lib/notifications/DNotificationContainer.svelte";
+</script>
+
+<Box>
+  <Header />
+  <Separator />
+  <UrlField />
+</Box>
+
+<DNotificationContainer />
+<Statusbar />
+
+<style>
+  :root {
+    background-color: black;
+  }
+</style>
