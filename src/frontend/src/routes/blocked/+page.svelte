133205008e93be0977f1a4286db0fae61f2dd481
Mark Devenyi <markdevenyidev@gmail.com>
2024-07-08 03:42:26 -0400

    Fix redirection in `blocked` page

--- src/frontend/src/routes/blocked/+page.svelte
+++ src/frontend/src/routes/blocked/+page.svelte
@@ -1,24 +1,35 @@
+<script lang="ts">
+  function redirect() {
+    window.location.href = "/";
+  }
+</script>
+
 <h1>Blocked</h1>
 <p>You are not logged in!</p>
-<a href="/login">Login here</a>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+<p class="redirect" on:click={() => redirect()}>Login here</p>
 
 <style>
-	h1 {
-		color: whitesmoke;
-		font-family: monaco, Consolas, "Lucida Console", monospace;
-	}
+  h1 {
+    color: whitesmoke;
+    font-family: monaco, Consolas, "Lucida Console", monospace;
+  }
 
-	p {
-		color: whitesmoke;
-		font-family: monaco, Consolas, "Lucida Console", monospace;
-	}
+  p {
+    color: whitesmoke;
+    font-family: monaco, Consolas, "Lucida Console", monospace;
+  }
 
-	a {
-		color: rgb(102, 153, 255);
-		font-family: monaco, Consolas, "Lucida Console", monospace;
-	}
+  .redirect {
+    margin-top: 0px;
+    color: rgb(102, 153, 255);
+    font-family: monaco, Consolas, "Lucida Console", monospace;
+    cursor: pointer;
+  }
 
-	:root {
-		background-color: black;
-	}
+  :root {
+    background-color: black;
+  }
 </style>
