ea4ad38580b7e88c8ee7efd0b3ac75ff9bc18ce7
Wrench56 <dmarkreg@gmail.com>
2024-07-08 02:50:43 -0400

    Add `dne` page

---
+++ src/frontend/src/routes/dne/+page.svelte
@@ -0,0 +1,18 @@
+<h1>Resource does not exist</h1>
+<p>The resource you tried to access does not exist!</p>
+
+<style>
+	h1 {
+		color: whitesmoke;
+		font-family: monaco, Consolas, "Lucida Console", monospace;
+	}
+
+	p {
+		color: whitesmoke;
+		font-family: monaco, Consolas, "Lucida Console", monospace;
+	}
+
+	:root {
+		background-color: black;
+	}
+</style>
